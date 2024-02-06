3623dfb6cbe18026168663b0c12ba34554c3ba3e
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2024-02-05 17:42:06 -0700

    Removed versioneer since it doesnt work 90% of the time. Falling back to __version__ in the __init__.py file.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from runpy import run_path
 from setuptools import find_packages, setup
 
 # read the program version from version.py (without loading the module)
-#__version__ = run_path('src/muller_idle/version.py')['__version__']
-import versioneer
+# __version__ = run_path('src/muller_idle/version.py')['__version__']
+from src.muller_idle import __version__
 
 
 def read(fname):
@@ -15,23 +15,24 @@ def read(fname):
 
 setup(
     name="muller-idle",
-    version=versioneer.get_version(),
     author="Alejandro Gil",
+    version=__version__,
     author_email="alejandrogilelias940711@gmail.com",
     description="Muller Idle takes the core of what makes an adventure game so addictive and strips it down to its purest form!",
     license="proprietary",
     url="",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    package_data={'muller_idle': ['res/*']},
-    long_description=read('README.md'),
-    install_requires=[],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pre-commit',
+    package_data={"muller_idle": ["res/*"]},
+    long_description=read("README.md"),
+    install_requires=[
+        "setuptools",
     ],
-    platforms='any',
-    python_requires='>=3.8',
-    cmdclass=versioneer.get_cmdclass(),
+    tests_require=[
+        "pytest",
+        "pytest-cov",
+        "pre-commit",
+    ],
+    platforms="any",
+    python_requires=">=3.8",
 )
--- src/muller_idle/__init__.py
+++ src/muller_idle/__init__.py
@@ -3,7 +3,8 @@ Muller Idle
 
 Muller Idle takes the core of what makes an adventure game so addictive and strips it down to its purest form!
 """
+
 __title__ = "muller-idle"
 __copyright__ = "© 2024 Sygil-Dev"
 
-from ._version import __version__
+__version__ = "0.0.1"
